3b0267df3585d85d08978755b643f53e36d54d9f
imhoward <howardian58@gmail.com>
2024-11-29 18:00:56 -0500

    Add calculator functionality
    The general functionality for the calculator has the user click on the
    keys in order to activiate event listeners. The event listeners add the
    numbers of the keys to the screen and once the user clicks on an operator
    that number is saved into a varible and the specific operator is saved.
    The user can then select more number keys, which will replace the
    previous number on the screen. A boolean is used in this case to detect
    if an operator was just selected or not. If so, the new number will
    replace the current number on the screen, otherwise, it will be tacked
    onto it. When the equals operator was clicked, the second number
    would be stored in another variable which would be passed into the
    operate function which would call the corresonding operation function
    (add, subtract, etc).
    
    In order to prevent the screen from overflowing, the string length for
    the number box was limited to 8. In order to accomodate for decimals,
    the method toFixed() was used if the resulting number had a decimal after
    it and would round the decimal to fit the screen.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,11 @@
+let firstNum;
+let sign;
+let secondNum;
+let signClicked;
+let firstClicked = 0;
+
+// Creation of the calculator body/layout
+
 let mainContainer = document.createElement("div");
 mainContainer.classList.add("main-container");
 
@@ -8,6 +16,10 @@ let numberBox = document.createElement("div");
 numberBox.classList.add("number-box");
 mainContainer.append(numberBox);
 
+let expressionBox = document.createElement("div");
+expressionBox.classList.add("expression-box");
+numberBox.append(expressionBox);
+
 let keypad = document.createElement("div");
 keypad.classList.add("keypad");
 mainContainer.append(keypad);
@@ -17,11 +29,24 @@ numKeys.classList.add("numKeys");
 numKeys.classList.add("key-containers");
 keypad.append(numKeys);
 
-let zeroKey = document.createElement("div");
+let zeroKey = document.createElement("button");
 zeroKey.classList.add("zeroKey");
 zeroKey.classList.add("key-containers");
 zeroKey.classList.add("keys");
 zeroKey.textContent = 0;
+
+zeroKey.addEventListener('click', function(e) {
+    if (firstClicked == 1) {
+        numberBox.textContent = 0;
+        firstClicked = 0;
+        //secondNum = i+1;
+    } else {
+        if ((numberBox.textContent).length < 8 ) {
+            numberBox.textContent += 0;
+        }
+    }
+});
+
 keypad.append(zeroKey);
 
 
@@ -31,19 +56,126 @@ signKeys.classList.add("key-containers");
 keypad.append(signKeys);
 
 for (let i = 0; i < 9; i++) {
-    let numKey = document.createElement("div");
+    let numKey = document.createElement("button");
     numKey.classList.add("numKey");
     numKey.classList.add("keys");
     numKey.textContent = i+1;
+
+    numKey.addEventListener('click', function(e) {
+        if (firstClicked == 1) {
+            numberBox.textContent = i+1;
+            firstClicked = 0;
+        } else {
+            if ((numberBox.textContent).length < 8 ) {
+                numberBox.textContent += i+1;
+            }
+        }
+    });
+
     numKeys.append(numKey);
 }
 
-const signs = ['+', '-', '×', '÷'];
+const signs = ['+', '-', '×', '÷', '='];
 
-for (let i = 0; i < 4; i++) {
-    let signKey = document.createElement("div");
+for (let i = 0; i < 5; i++) {
+    let signKey = document.createElement("button");
     signKey.classList.add("signKey");
     signKey.classList.add("keys");
     signKey.textContent = signs[i];
+
+    if (i < 4) {
+        signKey.addEventListener('click', function(e) {
+            signMode = 1;
+            firstNum = parseInt(numberBox.textContent);
+            firstClicked = 1;
+            signClicked = signs[i];
+        });
+    } else {
+        signKey.addEventListener('click', function(e) {
+            secondNum = parseInt(numberBox.textContent);
+            operate(firstNum, secondNum, signClicked);
+        });
+    }
+
     signKeys.append(signKey);
+}
+
+let clearKey = document.createElement("button");
+clearKey.classList.add("clear-key");
+clearKey.classList.add("keys");
+clearKey.textContent = 'C';
+
+clearKey.addEventListener('click', function(e) {
+    numberBox.textContent = null;
+    firstNum = 0;
+    secondNum = 0;
+    signClicked = null;
+    signMode = 0;
+});
+
+keypad.append(clearKey);
+
+// Calculator functionality
+
+function add(x, y) {
+    return x + y;
+}
+
+function subtract(x, y) {
+    return x - y;
+}
+
+function multiply(x, y) {
+    return x * y;
+}
+
+function divide(x, y) {
+    return x / y;
+}
+
+function operate(x, y, sign) {
+    if (sign == '+') {
+        let result = add(x,y);
+        if (result.toString().length > 8) {
+            numberBox.textContent = "NaN";
+            firstClicked = 1;
+        } else {
+            numberBox.textContent = result;
+        }
+    }
+    else if (sign == '-') {
+        let result = subtract(x,y);
+        if (result.toString().length > 8) {
+            numberBox.textContent = "NaN";
+            firstClicked = 1;
+        } else {
+            numberBox.textContent = result;
+        }
+    }
+    else if (sign == '×') {
+        let result = multiply(x,y);
+        if (result.toString().length > 8) {
+            numberBox.textContent = "NaN";
+            firstClicked = 1;
+        } else {
+            numberBox.textContent = result;
+        }
+    }
+    else if (sign == '÷') {
+        let result = divide(x,y);
+        if (result.toString().length > 8 && result % 1 == 0) {
+            numberBox.textContent = "NaN";
+            firstClicked = 1;
+        } 
+        else if (result.toString().length > 8 && result % 1 != 0) {
+            let decimals = result % 1;
+            let wholes = result - decimals;
+            if (decimals.toString().length > 6) {
+                numberBox.textContent = result.toFixed(6-(wholes.toString().length-1));
+            }
+        }
+        else {
+            numberBox.textContent = result;
+        }
+    }
 }
